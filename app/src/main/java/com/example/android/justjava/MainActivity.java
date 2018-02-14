6f26f84cf17023e717cae41eb589252fc1d48143
Apitos <apitos@gmail.com>
2018-02-15 00:42:05 +0100

    CheckBox -> Whipped Cream

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -12,7 +12,9 @@ import java.text.NumberFormat;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -23,6 +25,7 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,12 +54,13 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Create order Summury
      */
-    private String createOrderSummuty(int price) {
-        String message = "Name:Kaptain Kunal";
-        message += "\nQuantity : " + quantity;
-        message += "\nTotal $" + price;
-        message += "\nThank you!";
-        return message;
+    private String createOrderSummuty(int price, boolean isWhippedCreamChecked) {
+        String priceMessage = "Name:Lyla the labyrinth";
+        priceMessage += "\nAdd whipped cream ? " + isWhippedCreamChecked;
+        priceMessage += "\nQuantity : " + quantity;
+        priceMessage += "\nTotal $" + price;
+        priceMessage += "\nThank you!";
+        return priceMessage;
     }
 
     /**
@@ -64,12 +68,23 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
         /*int price = quantity * 5;*/
-        int price = calculatePrice();
 
+        int price = calculatePrice();
+        CheckBox whippedCreamCheckbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckbox.isChecked();
+        Log.v("MainActivity","Whipped is : " + hasWhippedCream );
+        /*if (whippedCreamCheckbox.isChecked())
+            // do something
+            isClicked = true;
+
+        else
+            isClicked = false;*/
+        // do something else
+        //Log.i("MainActivity", "Value of isClicked : " + isClicked);
         /*String priceMessage = "Total : $" + price + "\nThank you";*/
         /*priceMessage = priceMessage + "thank you";*/
 
-        displayMessage(createOrderSummuty(price));
+        displayMessage(createOrderSummuty(price,hasWhippedCream));
         //displayPrice(price);
 
     }
@@ -80,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param quantity    is the number of cups of coffee ordered
      * @param pricePerCup is the price of per cup
-     *  Return calculate price of cups
+     *                    Return calculate price of cups
      */
     private int calculatePrice() {
         /*int price = quantity * 5;
@@ -104,7 +119,6 @@ public class MainActivity extends AppCompatActivity {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }*/
-
     private void displayMessage(String message) {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(message);
